72185957781c9e53ea8276966f83ca7d85f0132b
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2023-07-04 18:44:29 +0000

    ES6_data_manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,19 @@
-function getListStudents() {
-  return [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
-    { id: 2, firstName: "James", location: "Columbia" },
-    { id: 5, firstName: "Serena", location: "San Francisco" }
-  ];
-}
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
+];
 
-const students = getListStudents();
-console.log(students);
+export default getListStudents;
